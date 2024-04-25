17c47c4b0e403c36ef014749701e85e2d183f87b
zhy <77427351+Zhanghaoyun24@users.noreply.github.com>
2024-04-25 11:04:56 +0800

    Update step1.py

--- video_processing/step1.py
+++ video_processing/step1.py
@@ -1,11 +1,12 @@
 import os
 """
-将6个10s片段处理为Q1、Q2、Q3、S1、S2、T1、T2四种类型
+LS fragments were processed into seven types: 
+Q1, Q2, Q3, S1, S2, T1, T2
 """
 video_folder = "./final_video/"
 qp = [48, 40, 29]
 videos = os.listdir(video_folder)
-# 设置不同QP level
+# Generate different QP levels
 for i in range(len(qp)):
     for video in videos:
         if video.endswith('.mp4'):
@@ -13,7 +14,7 @@ for i in range(len(qp)):
                 os.makedirs(f'./final_video/{video[:-5]}_QP')
             os.system(
                 f'ffmpeg -i final_video/{video} -c:v libx264 -qp {qp[i]} final_video/{video[:-5]}_QP/{video[:-4]}_Q{i + 1}.mp4')
-# 设置不同分辨率
+# Generate different resolution levels
 resulotion = ['640:480', '1280:960']
 for i in range(len(resulotion)):
     for video in videos:
@@ -22,7 +23,7 @@ for i in range(len(resulotion)):
                 os.makedirs(f'./final_video/{video[:-5]}_Res')
             os.system(
                 f'ffmpeg -i final_video/{video} -vf scale={resulotion[i]} final_video/{video[:-5]}_Res/{video[:-4]}_S{i + 1}.mp4')
-# 设置不同帧率
+# Generate different frame rate levels
 fps = [5, 10]
 for i in range(len(fps)):
     for video in videos:
